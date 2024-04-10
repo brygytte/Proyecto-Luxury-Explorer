1563fb6aa883e48cd1a965f601add19e7b1ceac9
brygytte <166582079+brygytte@users.noreply.github.com>
2024-04-09 22:41:40 -0600

    primer comit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const cardBora = document.getElementById("cardBora")
+
+cardBora.addEventListener("click",function(){
+    location.href="http://127.0.0.1:5500/index/continentes.html"
+})
+})
